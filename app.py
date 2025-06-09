cd29b065349d3db4c7bcb6a3f9607d5aa3a92044
Anca-Afloroaei <anca.afloroaei@gmail.com>
2025-06-09 10:09:47 +0200

    Created index route

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
+from flask import Flask, render_template
+import json
+import os
 
-from flask import Flask
 
 app = Flask(__name__)
 
 
+
+def load_posts():
+    if os.path.exists("blog_posts.json"):
+        with open("blog_posts.json", "r") as file:
+            return json.load(file)
+    return []
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
